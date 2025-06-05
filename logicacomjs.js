d08ceea34fb00833cf21ce20266b0a511ca20070
BMateZu <mateusbrabec976@gmail.com>
2025-06-05 16:51:36 -0300

    Aprendendo array

--- logicacomjs.js
+++ logicacomjs.js
@@ -27,7 +27,7 @@ else{
     alert("Recomendavel conteúdo senior")
 }*/
 
-let pontuacao = Number(prompt("Digite sua pontuação"))
+/* let pontuacao = Number(prompt("Digite sua pontuação"))
 if(pontuacao < 1000){
     alert("Você não possui bônus")
 }
@@ -42,4 +42,60 @@ else if(pontuacao <= 5001 && pontuacao >=10000) {
 }
 else{
     alert("Sua pontuação mais o bônus é"+(pontuacao + 2000),"seu nível é Mestre")
-}
+} */
+/* let planobasico = 25;
+let planopremiun = 40;
+let planoultra = 60;
+let telastresquatro = 5;
+let telasmaisdequatro = 7.50;
+
+let tipoassinatura = (prompt("Qual assinatura você deseja"))
+let quantitelas = prompt("Digite quantas telas você terá:")
+if(tipoassinatura == "básico"){
+    if(quantitelas>=3 && quantitelas<=4){
+        alert("O preço será " +(planobasico+telastresquatro))
+    }
+      else if(quantitelas>=4){
+        alert("O preço será " +(planobasico+telasmaisdequatro))
+    }
+    else{
+        alert("O preço será " +planobasico)
+    }
+}  
+else if(tipoassinatura == "premium"){
+    if(quantitelas>=3 && quantitelas<=4){
+        alert("O preço será " +(planopremiun+telastresquatro))
+    }
+      else if(quantitelas>=4){
+        alert("O preço será " +(planopremiun+telasmaisdequatro))
+    }
+    else{
+        alert("O preço será " +planopremiun)
+    }
+}  
+else {
+    if(quantitelas>=3 && quantitelas<=4){
+        alert("O preço será " +(planoultra+telastresquatro))
+    }
+      else if(quantitelas>=4){
+        alert("O preço será " +(planoultra+telasmaisdequatro))
+    }
+    else{
+        alert("O preço será " +ultra)
+    }
+} 
+    *//*
+let daysOFweek = ["Raquel", "Maraisa",80,40,10]; //iniciando um array 
+console.log(daysOFweek[1]) //ver por índice 
+console.log(daysOFweek.length) // ver quantos itens tem esse array
+let nomedoaluno = prompt("Digite o nome do novo aluno: ")//Adiciona no Array com push e prompt 
+daysOFweek.unshift(nomedoaluno) //Adionado ao fim do Array 
+daysOFweek.push("Miguel", "Alysson", "Arthur", "Nayline")
+daysOFweek.unshift("João")
+daysOFweek.pop()
+daysOFweek.shift()
+daysOFweek.sort()
+daysOFweek.reverse()
+alert("Atualmente os alunos da escola são: "+daysOFweek) //Usando alert e uma array
+alert(daysOFweek.indexOf("Arthur"))*/
+
